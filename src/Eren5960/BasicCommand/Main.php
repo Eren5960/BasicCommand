8c558619cd6bd39eeda82db9ffdd6830d7c32307
Eren A. Akyol <ahmederen123@gmail.com>
2019-01-18 15:01:18 +0300

    Initial commit

---
+++ src/Eren5960/BasicCommand/Main.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *  _____                    ____   ___    __     ___
+ * | ____| _ __  ___  _ __  | ___| / _ \  / /_   / _ \
+ * |  _|  | '__|/ _ \| '_ \ |___ \| (_) || '_ \ | | | |
+ * | |___ | |  |  __/| | | | ___) |\__, || (_) || |_| |
+ * |_____||_|   \___||_| |_||____/   /_/  \___/  \___/
+ *
+ * @author Eren5960
+ * @link https://github.com/Eren5960
+ */
+declare(strict_types=1);
+
+namespace Eren5960\BasicCommand;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onChat(PlayerChatEvent $event) : void{
+		$player = $event->getPlayer();
+		$message = $event->getMessage();
+
+		if(substr($message, 0, 2) === "./"){
+			$event->setCancelled();
+			$this->getServer()->dispatchCommand($player, substr($message, 2));
+		}
+	}
+}
